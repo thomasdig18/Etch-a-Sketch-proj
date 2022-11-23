738840fdde34365538d7d1b67f115b00faa1fdcb
Thomas DiGregorio <thomas.digregorio@gmail.com>
2022-11-23 10:40:39 -0500

    Fixed board styles to have display grid and also added function that creates the 256 sqaures in the 16x16 grid

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-let grid = document.querySelector('.grid');
+let board = document.querySelector(".board");
 
-grid.getElementsByClassName.gridTemplateColumns = 'repeat(16, 1fr)';
+board.style.gridTemplateColumns = "repeat(16 , 1fr)";
+board.style.gridTemplateRows = "repeat(16 , 1fr)";
+
+for(let i = 0; i < 256; i++) {
+    let square = document.createElement('div');
+    square.style.backgroundColor = 'blue';
+    board.insertAdjacentElement("beforeend", square);
+}
